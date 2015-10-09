1acf60eedd08d6039585986af101e386a6a3b5c1
Maycon Spirlandelli <mayconbatista8@gmail.com>
2015-10-09 13:54:30 -0300

    Alterado a classe Conta para implementar a Serializable e acrescentar o atributo 'id'.

--- app/src/main/java/br/ufg/inf/instaladoreduroam/entidades/Conta.java
+++ app/src/main/java/br/ufg/inf/instaladoreduroam/entidades/Conta.java
@@ -1,19 +1,31 @@
 package br.ufg.inf.instaladoreduroam.entidades;
 
+import java.io.Serializable;
+
 /**
  * Created by Maycon on 26/05/2015.
  */
-public class Conta {
+public class Conta implements Serializable {
 
+    private long id;
     private String login;
     private String senha;
 
     public Conta() {
     }
 
-    public Conta(String login, String senha) {
+    public Conta(long id, String login, String senha) {
+        this.id = id;
         this.login = login;
         this.senha = senha;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getLogin() {

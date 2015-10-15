776cb39c36c16c08f651b5774b90c387ac7e638b
Maycon Spirlandelli <mayconbatista8@gmail.com>
2015-10-15 14:35:56 -0300

    Incluido o toolbar na tela de Contas da classe 'ContasActivity'.

--- app/src/main/java/br/ufg/inf/instaladoreduroam/app/ContasActivity.java
+++ app/src/main/java/br/ufg/inf/instaladoreduroam/app/ContasActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -20,11 +21,18 @@ public class ContasActivity extends ActionBarActivity implements ContaListFragme
 
     private Button btnAdicionarRede;
     private ContaListFragment mListFragament;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contas);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_contas);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.title_fragment_conta);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
     }
 
     @Override

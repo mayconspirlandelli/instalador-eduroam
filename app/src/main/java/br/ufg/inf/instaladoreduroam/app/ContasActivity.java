1fcbbdb673f3366909f204274cbbc66fa017cab0
Maycon Spirlandelli <mayconbatista8@gmail.com>
2015-10-26 11:53:00 -0200

    Incluido o botão 'Adicionar nova Conta' na toolbar.

--- app/src/main/java/br/ufg/inf/instaladoreduroam/app/ContasActivity.java
+++ app/src/main/java/br/ufg/inf/instaladoreduroam/app/ContasActivity.java
@@ -49,22 +49,15 @@ public class ContasActivity extends ActionBarActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_new:
+                adicionarConta();
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-    private void inicializaObjetosDeTela() {
-    }
-
 
     @Override
     protected void onResume() {
